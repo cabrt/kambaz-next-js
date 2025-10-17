ae9415784039c1ac0e42b9fc7768f473c2c54cf8
conor-atACD <conor.abramson-tieu@austincircuit.com>
2025-10-17 16:12:50 -0400

    a3 JavaScript

--- app/Labs/Lab2/Float.tsx
+++ app/Labs/Lab2/Float.tsx
@@ -4,16 +4,16 @@ export default function Float() {
       <h2>Float</h2>
       <div>
         <img className="wd-float-right"
-          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="Starship"/>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic reprehenderit unde rem ipsam reiciendis placeat quae, iusto temporibus accusantium voluptatibus eveniet voluptates delectus autem velit. Itaque doloribus accusamus laudantium. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic reprehenderit unde rem ipsam reiciendis placeat quae, iusto temporibus accusantium voluptatibus eveniet voluptates delectus autem velit. Itaque doloribus accusamus laudantium.
         <img className="wd-float-left"
-          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="Starship"/>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic reprehenderit unde rem ipsam reiciendis placeat quae, iusto temporibus accusantium voluptatibus eveniet voluptates delectus autem velit. Itaque doloribus accusamus laudantium. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic reprehenderit unde rem ipsam reiciendis placeat quae, iusto temporibus accusantium voluptatibus eveniet voluptates delectus autem velit. Itaque doloribus accusamus laudantium.
         <img className="wd-float-right"
-          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="Starship"/>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic reprehenderit unde rem ipsam reiciendis placeat quae, iusto temporibus accusantium voluptatibus eveniet voluptates delectus autem velit. Itaque doloribus accusamus laudantium. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic reprehenderit unde rem ipsam reiciendis placeat quae, iusto temporibus accusantium voluptatibus eveniet voluptates delectus autem velit. Itaque doloribus accusamus laudantium.
         <img className="wd-float-left"
-          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="Starship"/>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic reprehenderit unde rem ipsam reiciendis placeat quae, iusto temporibus accusantium voluptatibus eveniet voluptates delectus autem velit. Itaque doloribus accusamus laudantium. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic reprehenderit unde rem ipsam reiciendis placeat quae, iusto temporibus accusantium voluptatibus eveniet voluptates delectus autem velit. Itaque doloribus accusamus laudantium.
         <div className="wd-float-done"></div>
       </div>
@@ -27,7 +27,7 @@ export default function Float() {
         <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
           Red </div>
         <img className="wd-float-right"
-          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="Starship"/>
         <div className="wd-float-done"></div>
       </div>
     </div>
